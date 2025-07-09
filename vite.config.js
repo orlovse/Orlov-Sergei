c604e5a987cd81a4dd71c7f3153cf653fbb632fc
Sergei Orlov <hajoff@gmail.com>
2025-07-09 11:40:34 +0300

    Add rollup-plugin-visualizer

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
-import glsl from 'vite-plugin-glsl';
-import eslint from 'vite-plugin-eslint'
 import { defineConfig } from 'vite';
+import glsl from 'vite-plugin-glsl';
+import eslint from 'vite-plugin-eslint';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
 	plugins: [
 		glsl(),
-		eslint()
-	]
+		eslint(),
+		visualizer({
+			open: true,
+			filename: 'stats.html',
+		}),
+	],
 });
