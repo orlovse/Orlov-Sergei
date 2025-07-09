84e26fb3a9dbe8e4751cadaa3672f28866d15a4f
Sergei Orlov <hajoff@gmail.com>
2025-07-09 11:21:31 +0300

    Add three js scene

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import './style.css'
+import Three from './js/app.js';
+
+document.addEventListener('DOMContentLoaded', () => {});
+
+window.addEventListener('load', () => {
+	const canvas = document.querySelector('#canvas');
+
+	if (canvas) {
+		new Three(document.querySelector('#canvas'));
+	}
+});
